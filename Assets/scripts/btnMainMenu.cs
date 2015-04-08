08d2e07ba90cecba26de718755829ba313b18ef3
SlippyTheToad <mr.majii@gmail.com>
2015-04-08 01:27:14 -0500

    Initial Commit
    This is the initial commit after I've been developing like an idiot
    without maintaining source control revisions. In this commit, you'll
    find the start of the menu process for Unity. The main menu, warning,
    and the basis of the redeveloped character creation is currently
    available.

---
+++ Assets/scripts/btnMainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class btnMainMenu : MonoBehaviour {
+	private bool ShowWarning = false;
+	public CanvasGroup cvsMenuButtons;
+	public CanvasGroup cvsWarning;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void guiToggle()
+	{
+		if (!ShowWarning) {
+			ShowWarning = true;
+			cvsMenuButtons.interactable = !cvsMenuButtons.interactable;
+			
+			cvsWarning.interactable = true;
+			cvsWarning.alpha = 1.0f;
+			cvsWarning.blocksRaycasts = true;
+		} else {
+			ShowWarning = false;
+			cvsMenuButtons.interactable = true;
+			
+			cvsWarning.interactable = false;
+			cvsWarning.alpha = 0.0f;
+			cvsWarning.blocksRaycasts = false;
+		}
+	}
+
+	public void NewGame()
+	{
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
